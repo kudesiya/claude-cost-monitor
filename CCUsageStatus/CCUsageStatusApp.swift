0268180a2da63d296aa40d64aedd47a967da674c
Gaurav Kudesiya <gaurav.kudesiya@outlook.com>
2026-06-03 21:19:36 -0700

    Initial Commit

---
+++ CCUsageStatus/CCUsageStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCUsageStatusApp.swift
+//  CCUsageStatus
+//
+//  Created by Gaurav Kudesiya on 6/3/26.
+//
+
+import SwiftUI
+
+@main
+struct CCUsageStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CCUsageStatus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CCUsageStatus
+//
+//  Created by Gaurav Kudesiya on 6/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
